2026df537da540f4317cbf282d41218e01b3c69f
ingring <ingrid.gladheim@student.uib.no>
2023-02-21 15:15:12 +0100

    jeg prøver igjen

--- database.py
+++ database.py
@@ -7,6 +7,6 @@ MONGOHOST = 'containers-us-west-152.railway.app'
 MONGOPORT = 5676
 
 
-client = MongoClient(os.getenv('mongodb://${{ MONGOUSER }}:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT }}'))
+client = MongoClient(os.getenv('mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}'))
 db = client.get_default_database()
 
