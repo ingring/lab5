759a05873ba1b7fd149c1eeff7209bca6fc9d482
ingring <ingrid.gladheim@student.uib.no>
2023-02-21 15:28:44 +0100

    prøver igjen

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 import os
 
-MONGOUSER = 'mongo'
-MONGOPASSWORD = 'Oqzfv0GRj9ha34InGfXP'
-MONGOHOST = 'containers-us-west-152.railway.app'
-MONGOPORT = 5676
+# MONGOUSER = 'mongo'
+# MONGOPASSWORD = 'Oqzfv0GRj9ha34InGfXP'
+# MONGOHOST = 'containers-us-west-152.railway.app'
+# MONGOPORT = 5676
 
 
-client = MongoClient(os.getenv('mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}'))
+client = MongoClient('mongodb://mongo:Oqzfv0GRj9ha34InGfXP@containers-us-west-152.railway.app:5676')
 db = client.get_default_database()
 
